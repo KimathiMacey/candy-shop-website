38bb603a6442d1c8bd2160db8984334f0c6d3070
Macey Kimathi <87859886+KimathiMacey@users.noreply.github.com>
2023-11-19 20:20:34 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,89 +1,208 @@
+// ************************************************
+// Shopping Cart API
+// ************************************************
 
+var shoppingCart = (function() {
+  // =============================
+  // Private methods and properties
+  // =============================
+  cart = [];
+  
+  // Constructor
+  function Item(name, price, count) {
+    this.name = name;
+    this.price = price;
+    this.count = count;
+  }
+  
+  // Save cart
+  function saveCart() {
+    sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
+  }
+  
+    // Load cart
+  function loadCart() {
+    cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
+  }
+  if (sessionStorage.getItem("shoppingCart") != null) {
+    loadCart();
+  }
+  
 
-for (let i of products.data) {
-  //Create Card
-  let card = document.createElement("div");
-  //Card should have category and should stay hidden initially
-  card.classList.add("card", i.category, "hide");
-  //image div
-  let imgContainer = document.createElement("div");
-  imgContainer.classList.add("image-container");
-  //img tag
-  let image = document.createElement("img");
-  image.setAttribute("src", i.image);
-  imgContainer.appendChild(image);
-  card.appendChild(imgContainer);
-  //container
-  let container = document.createElement("div");
-  container.classList.add("container");
-  //product name
-  let name = document.createElement("h5");
-  name.classList.add("product-name");
-  name.innerText = i.productName.toUpperCase();
-  container.appendChild(name);
-  //price
-  let price = document.createElement("h6");
-  price.innerText = "$" + i.price;
-  container.appendChild(price);
-
-  card.appendChild(container);
-  document.getElementById("products").appendChild(card);
-}
-
-//parameter passed from button (Parameter same as category)
-function filterProduct(value) {
-  //Button class code
-  let buttons = document.querySelectorAll(".button-value");
-  buttons.forEach((button) => {
-    //check if value equals innerText
-    if (value.toUpperCase() == button.innerText.toUpperCase()) {
-      button.classList.add("active");
-    } else {
-      button.classList.remove("active");
-    }
-  });
-
-  //select all cards
-  let elements = document.querySelectorAll(".card");
-  //loop through all cards
-  elements.forEach((element) => {
-    //display all cards on 'all' button click
-    if (value == "all") {
-      element.classList.remove("hide");
-    } else {
-      //Check if element contains category class
-      if (element.classList.contains(value)) {
-        //display element based on category
-        element.classList.remove("hide");
-      } else {
-        //hide other elements
-        element.classList.add("hide");
+  // =============================
+  // Public methods and properties
+  // =============================
+  var obj = {};
+  
+  // Add to cart
+  obj.addItemToCart = function(name, price, count) {
+    for(var item in cart) {
+      if(cart[item].name === name) {
+        cart[item].count ++;
+        saveCart();
+        return;
       }
     }
-  });
-}
-
-//Search button click
-document.getElementById("search").addEventListener("click", () => {
-  //initializations
-  let searchInput = document.getElementById("search-input").value;
-  let elements = document.querySelectorAll(".product-name");
-  let cards = document.querySelectorAll(".card");
-
-  //loop through all elements
-  elements.forEach((element, index) => {
-    //check if text includes the search value
-    if (element.innerText.includes(searchInput.toUpperCase())) {
-      //display matching card
-      cards[index].classList.remove("hide");
-    } else {
-      //hide others
-      cards[index].classList.add("hide");
+    var item = new Item(name, price, count);
+    cart.push(item);
+    saveCart();
+  }
+  // Set count from item
+  obj.setCountForItem = function(name, count) {
+    for(var i in cart) {
+      if (cart[i].name === name) {
+        cart[i].count = count;
+        break;
+      }
     }
-  });
+  };
+  // Remove item from cart
+  obj.removeItemFromCart = function(name) {
+      for(var item in cart) {
+        if(cart[item].name === name) {
+          cart[item].count --;
+          if(cart[item].count === 0) {
+            cart.splice(item, 1);
+          }
+          break;
+        }
+    }
+    saveCart();
+  }
+
+  // Remove all items from cart
+  obj.removeItemFromCartAll = function(name) {
+    for(var item in cart) {
+      if(cart[item].name === name) {
+        cart.splice(item, 1);
+        break;
+      }
+    }
+    saveCart();
+  }
+
+  // Clear cart
+  obj.clearCart = function() {
+    cart = [];
+    saveCart();
+  }
+
+  // Count cart 
+  obj.totalCount = function() {
+    var totalCount = 0;
+    for(var item in cart) {
+      totalCount += cart[item].count;
+    }
+    return totalCount;
+  }
+
+  // Total cart
+  obj.totalCart = function() {
+    var totalCart = 0;
+    for(var item in cart) {
+      totalCart += cart[item].price * cart[item].count;
+    }
+    return Number(totalCart.toFixed(2));
+  }
+
+  // List cart
+  obj.listCart = function() {
+    var cartCopy = [];
+    for(i in cart) {
+      item = cart[i];
+      itemCopy = {};
+      for(p in item) {
+        itemCopy[p] = item[p];
+
+      }
+      itemCopy.total = Number(item.price * item.count).toFixed(2);
+      cartCopy.push(itemCopy)
+    }
+    return cartCopy;
+  }
+
+  // cart : Array
+  // Item : Object/Class
+  // addItemToCart : Function
+  // removeItemFromCart : Function
+  // removeItemFromCartAll : Function
+  // clearCart : Function
+  // countCart : Function
+  // totalCart : Function
+  // listCart : Function
+  // saveCart : Function
+  // loadCart : Function
+  return obj;
+})();
+
+
+// *****************************************
+// Triggers / Events
+// ***************************************** 
+// Add item
+$('.add-to-cart').click(function(event) {
+  event.preventDefault();
+  var name = $(this).data('name');
+  var price = Number($(this).data('price'));
+  shoppingCart.addItemToCart(name, price, 1);
+  displayCart();
 });
 
-//Initially display all products
-window.onload = () => {
-  filterProduct("all");
-};
+// Clear items
+$('.clear-cart').click(function() {
+  shoppingCart.clearCart();
+  displayCart();
+});
+
+
+function displayCart() {
+  var cartArray = shoppingCart.listCart();
+  var output = "";
+  for(var i in cartArray) {
+    output += "<tr>"
+      + "<td>" + cartArray[i].name + "</td>" 
+      + "<td>(" + cartArray[i].price + ")</td>"
+      + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
+      + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
+      + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
+      + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
+      + " = " 
+      + "<td>" + cartArray[i].total + "</td>" 
+      +  "</tr>";
+  }
+  $('.show-cart').html(output);
+  $('.total-cart').html(shoppingCart.totalCart());
+  $('.total-count').html(shoppingCart.totalCount());
+}
+
+// Delete item button
+
+$('.show-cart').on("click", ".delete-item", function(event) {
+  var name = $(this).data('name')
+  shoppingCart.removeItemFromCartAll(name);
+  displayCart();
+})
+
+
+// -1
+$('.show-cart').on("click", ".minus-item", function(event) {
+  var name = $(this).data('name')
+  shoppingCart.removeItemFromCart(name);
+  displayCart();
+})
+// +1
+$('.show-cart').on("click", ".plus-item", function(event) {
+  var name = $(this).data('name')
+  shoppingCart.addItemToCart(name);
+  displayCart();
+})
+
+// Item count input
+$('.show-cart').on("change", ".item-count", function(event) {
+   var name = $(this).data('name');
+   var count = Number($(this).val());
+  shoppingCart.setCountForItem(name, count);
+  displayCart();
+});
+
+displayCart();
